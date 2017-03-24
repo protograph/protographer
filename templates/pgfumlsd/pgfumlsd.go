ce9c156fc8ec28b707f33d13c941f6e52763ffc0
Albert Yu <yukinying@gmail.com>
2017-03-24 00:37:50 -0700

    expand footnote

--- templates/pgfumlsd/pgfumlsd.go
+++ templates/pgfumlsd/pgfumlsd.go
@@ -142,7 +142,7 @@ const theTemplate = `
       ##- end ##
     ##- end##
     ##- if .FootNote ##
-\node [anchor=north west] at ([yshift=-20] current bounding box.south west){## .FootNote ##};
+\node [anchor=north west] at ([yshift=-20] current bounding box.south west){## expand .FootNote ##};
     ##- end##
 ##- end ##
 `
